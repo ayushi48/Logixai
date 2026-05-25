68f77a3a1a876aa41017097fda06b4b61d643ece
AYUSHI KUMARI <162792850+ayushi48@users.noreply.github.com>
2026-05-25 12:48:02 +0530

    Refactor axiosClient setup and remove comments

--- Logixaii/frontend/src/utils/axioClient.jsx
+++ Logixaii/frontend/src/utils/axioClient.jsx
@@ -1,41 +1,11 @@
 
-// import axios from "axios";
-
-// const axiosClient = axios.create({
-// baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
-//   withCredentials: true,   // 🔑 ensures cookies (accessToken) are sent
-// });
-
-// export default axiosClient;
-
-
-
-
-
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
-  withCredentials: true,
+baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+  withCredentials: true,   // 🔑 ensures cookies (accessToken) are sent
 });
 
-// ✅ Add this below
-axiosClient.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      try {
-        await axiosClient.post("/auth/refresh");
-        return axiosClient(originalRequest);
-      } catch (err) {
-        window.location.href = "/";
-      }
-    }
-    return Promise.reject(error);
-  }
-);
-
 export default axiosClient;
+
+
